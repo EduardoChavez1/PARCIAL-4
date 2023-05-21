804796cc03bc29e3516ee15c991bf7a940b6bb0a
GECB1 <123275704+GECB1@users.noreply.github.com>
2023-05-20 19:12:11 -0600

    25-0250-2017
    Parcial 4

---
+++ Parcial 4/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebaseflutter/principal.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FirebaseAPP());
+}
+
+class FirebaseAPP extends StatelessWidget {
+  const FirebaseAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Parcial 4 25-0250-2017',
+      home: Principal(),
+    );
+  }
+}
+
